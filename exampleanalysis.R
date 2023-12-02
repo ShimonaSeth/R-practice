1cc25bf6e7c8eb8ab00bf9549971ef4e74e50611
ShimonaSeth <147535863+ShimonaSeth@users.noreply.github.com>
2023-12-02 14:15:54 +0100

    Cleaned the data and modified it a bit in r script file

--- exampleanalysis.R
+++ exampleanalysis.R
@@ -1,1 +1,19 @@
-print("Hello world")
+# loading packages required to clean data
+library(tidyverse)
+
+#taking the glimpse() to view the column names and data types dataset
+glimpse(Adverseevent)
+
+#cleaning the dataset
+
+#loading the csv file
+data <- read.csv("Adverseevent.csv")
+head(data)
+
+#converting uppercase to lower case
+data$County  <- tolower(data$County)
+head(data)  
+
+#saving the modified data
+write.csv(data,"modified_adverseevent.csv")
+head(data)
